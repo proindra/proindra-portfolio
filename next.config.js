d9b064c0c715279512127d84a16187c52f9e7bf0
proindra <prajwalindrakh@gmail.com>
2026-06-03 12:53:37 +0530

    Add .env.local for dev, use NEXT_PUBLIC_BASEPATH_DISABLED for localhost support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_DEPLOY_URL === "true" ? "/proindra_portfolio" : "",
+  basePath: process.env.NEXT_PUBLIC_BASEPATH_DISABLED === "true" ? "" : "/proindra_portfolio",
   output: "export",
   images: {
     remotePatterns: [
